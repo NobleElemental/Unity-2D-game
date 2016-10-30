812dcb2ea83470393716dd2d88f35910d806329d
John Cetin <johnc@Johns-MacBook-Pro-5.local>
2016-10-29 21:50:19 -0400

    Added Pictures/Scripts/Documentation
    Added ships and background.
    player and background script added
    External Documentation included in project folder

---
+++ Assignment 1/Assets/Scripts/backgroundController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class backgroundController : MonoBehaviour {
+
+	[SerializeField]
+	private float speed;
+
+	private Transform _transform;
+	private Vector2 _currentPosition;
+
+	// Use this for initialization
+	void Start () {
+		_transform = gameObject.GetComponent<Transform>();
+		_currentPosition = _transform.position;
+		Reset ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		_currentPosition = _transform.position;
+
+		_currentPosition -= new Vector2 (speed, 0);
+		_transform.position = _currentPosition;
+
+		if (_currentPosition.x <= -38.35f) {
+			Reset ();
+		}
+	}
+
+	private void Reset(){
+
+		_currentPosition = new Vector2 (-19.15f, 0);
+		_transform.position = _currentPosition;
+	}
+}
---
+++ Assignment 1/Assets/Scripts/playerController.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerController : MonoBehaviour {
+
+	[SerializeField]
+	private float speed = 0;
+
+	private Transform _transform;
+	private Vector2 _currentPosition;
+	private float inputX;
+	private float inputY;
+
+	// Use this for initialization
+	void Start () {
+
+		_transform = gameObject.GetComponent<Transform> ();
+		_currentPosition = _transform.position;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		inputX = Input.GetAxis ("Horizontal");
+		inputY = Input.GetAxis ("Vertical");
+		_currentPosition = _transform.position;
+
+		if (inputX > 0) {
+			_currentPosition += new Vector2 (speed, 0);
+		}
+
+		if (inputX < 0) {
+			_currentPosition -= new Vector2 (speed, 0);
+		}
+
+		if (inputY > 0) {
+			_currentPosition += new Vector2 (0, speed);
+		}
+
+		if (inputY < 0) {
+			_currentPosition -= new Vector2 (0, speed);
+		}
+
+		checkBounds ();
+		_transform.position = _currentPosition;
+	
+	}
+
+	private void checkBounds(){
+
+		if(_currentPosition.x < -38f){
+			_currentPosition.x = -38f;
+		}
+		if(_currentPosition.x >-32.4f){
+			_currentPosition.x = -32.4f;
+		}
+
+		if(_currentPosition.y < -1.45f) {
+			_currentPosition.y = -1.45f;
+		}
+
+		if (_currentPosition.y > 1.45f) {
+			_currentPosition.y = 1.45f;
+		}
+	}
+}
